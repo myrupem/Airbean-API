bf200ae2e2abc2971cab41f0846110828d430c4a
KesiiR <rebinyoussef2@gmail.com>
2025-05-30 10:00:25 +0200

    Updated server.js with MongoDB connection

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// configuration
+dotenv.config(); // Detta laddar miljövariabler från en .env-fil
+const app = express(); // Detta skapar upp en express-applikation
+const PORT = process.env.PORT;
+mongoose.connect(process.env.CONNECTION_STRING);
+const database = mongoose.connection;
+
+// middleware
+app.use(express.json());
+
+// routes
+
+database.on("error", (error) => {
+  console.error("Database connection error:", error);
+});
+
+database.once("connected", () => {
+  console.log("Database connected");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
